a97ef87da519f3d0bc7a26dbce32c390ea1bc5bb
Yue <yue.wang>
2023-06-09 16:02:32 +0800

    cors

--- enx-api/deploy.sh
+++ enx-api/deploy.sh
@@ -3,10 +3,10 @@ package_name="enx-api"
 echo "building"
 GOOS=linux GOARCH=amd64 go build -o ${package_name} enx-api.go
 echo "stop service"
-ansible -i 'wiloon.com,' all  -m shell -a 'systemctl stop enx' -u root
-scp ${package_name} -i ~/.ssh/id_ed25519_w10n aliyun:/usr/local/bin
-scp config-aliyun.toml -i ~/.ssh/id_ed25519_w10n aliyun:/usr/local/etc/enx-api.toml
+ansible -i 'wiloon.com,' all -m shell -a 'systemctl stop enx-api' -u root --key-file ~/.ssh/id_ed25519_w10n
+scp -i ~/.ssh/id_ed25519_w10n ${package_name} aliyun:/usr/local/bin
+scp -i ~/.ssh/id_ed25519_w10n config-aliyun.toml aliyun:/usr/local/etc/enx-api.toml
 rm -f ${package_name}
 
 echo "start service"
-ansible -i 'wiloon.com,' all  -m shell -a 'systemctl start enx-api' -u root
+ansible -i 'wiloon.com,' all  -m shell -a 'systemctl restart enx-api' -u root --key-file ~/.ssh/id_ed25519_w10n
