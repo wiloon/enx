beb7f475f34ba82c850557328640d2eee0d693e5
Yue <wiloon.wy@gmail.com>
2024-09-23 18:42:39 +0800

    monthly commit

--- chrome-enx/content_module.js
+++ chrome-enx/content_module.js
@@ -19,34 +19,40 @@ export function findChildNodes0(parentNode){
 
     console.log("child node count: ", childNodes.length)
     if (childNodes.length === 0) {
-        return
+        console.log("child node count ==0 return")
+        return nodeList
     }
+
     // check if article context
     let articleContent = true
-    for (let node of childNodes) {
-        let tagName = node.tagName
-        console.log("child node: ", node, ", tag name: ", tagName)
+    for (let childNode of childNodes) {
+        let childNodeTagName = childNode.tagName
+        console.log("child node: ", childNode, ", tag name: ", childNodeTagName)
 
         // if child node only contains A, CODE ..., this node is article content node
-        if (tagName !== "A" && tagName !== undefined && tagName !== "EM" && tagName !== "CODE") {
+        if (childNodeTagName !== "A" && childNodeTagName !== undefined && childNodeTagName !== "EM" && childNodeTagName !== "CODE") {
             // has sub article node
             articleContent = false
+            console.log("article content: ", articleContent)
             break
         }
     }
 
+    let index = 0
     if (articleContent === false) {
         console.log("article node ==false, check sub node")
         for (let node of childNodes) {
+            console.log("check child node, index: ", index)
             let tmp_list = findChildNodes0(node)
             nodeList = nodeList.concat(tmp_list)
+            index = index + 1
         }
         return nodeList
     }
 
 
-    let tagName = parentNode.tagName
-    console.log("tag: ", tagName, "node: ", parentNode)
+    let currentTagName = parentNode.currentTagName
+    console.log("current tag: ", currentTagName, "node: ", parentNode)
 
     console.log("span width: ", parentNode.getBoundingClientRect().width)
     if (parentNode.getBoundingClientRect().width > spanWidth) {
