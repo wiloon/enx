5f0baa977bea8ac1a2f2532311fd501ab462e79a
Yue <wiloon.wy@gmail.com>
2024-10-05 20:00:56 +0800

    infq text node

--- chrome-enx/content.js
+++ chrome-enx/content.js
@@ -100,152 +100,6 @@ function getColorCodeByCount(ecp) {
 
 let spanWidth = 0;
 
-function findChildNodes(parentNode){
-    let nodeList = []
-
-    console.log("find child node 0, parent node: ", parentNode)
-    let childNodes = parentNode.childNodes
-
-    console.log("child node count: ", childNodes.length)
-    if (childNodes.length === 0) {
-        return
-    }
-    // check if article context
-    let articleContent = true
-    for (let node of childNodes) {
-        let tagName = node.tagName
-        console.log("child node: ", node, ", tag name: ", tagName)
-
-        // if child node only contains A, CODE ..., this node is article content node
-        if (tagName !== "A" && tagName !== undefined && tagName !== "EM" && tagName !== "CODE") {
-            // has sub article node
-            articleContent = false
-            break
-        }
-    }
-
-    if (articleContent === false) {
-        console.log("article node ==false, check sub node")
-        for (let node of childNodes) {
-            let tmp_list = findChildNodes(node)
-            nodeList = nodeList.concat(tmp_list)
-        }
-        return;
-    }
-
-
-    let tagName = parentNode.tagName
-    console.log("tag: ", tagName, "node: ", parentNode)
-
-    console.log("span width: ", parentNode.getBoundingClientRect().width)
-    if (parentNode.getBoundingClientRect().width > spanWidth) {
-        spanWidth = parentNode.getBoundingClientRect().width
-    }
-
-    let spanContent = parentNode.innerHTML
-    let oneParagraph = parentNode.textContent
-
-    // remove duplicate whitespace
-    oneParagraph = oneParagraph.replace(/\s+/g, ' ')
-    console.log('inner text: ', oneParagraph)
-    console.log('span inner html: ', spanContent)
-    console.log("span width: ", spanWidth)
-    // remove <a> tag
-    let filteredContent = ""
-    let tagAppeared = false
-    let collectWord = true
-    for (i = 0; i < spanContent.length; i++) {
-        if (!tagAppeared) {
-            collectWord = true
-        }
-        c = spanContent.charAt(i)
-        if (c === "<") {
-            // tag start
-            tagAppeared = true
-        } else if (c === ">") {
-            tagAppeared = false
-        }
-        if (tagAppeared) {
-            collectWord = false
-        }
-        if (collectWord) {
-            filteredContent += c
-        }
-    }
-    console.log("span inner html remove tag: ", filteredContent)
-    let words = filteredContent.split(' ')
-
-
-    let re = /^[0-9a-zA-Z-,.']+$/;
-
-    let rawParagraphWordArray = [];
-
-    for (let word of words) {
-        word = word.replace(",", "");
-        word = word.replace(".", "");
-        if (re.test(word)) {
-            rawParagraphWordArray.push(word)
-        }
-    }
-    // send word array to backend
-    // get word properties from backend
-    (async () => {
-        console.log("sending msg from content script to backend")
-        const response = await chrome.runtime.sendMessage({ msgType: "getWords", words: oneParagraph });
-        // do something with response here, not outside the function
-        console.log("response from backend: ", response)
-        console.log(response.wordProperties);
-
-        let newSpanContent = ""
-        let spanContentLength = 0
-        let wordMargin = 4;
-
-        wordsInParagraph = oneParagraph.split(' ')
-        for (let wordInRawParagraph of wordsInParagraph) {
-            if (spanContentLength > 0 && spanWidth > 50 && spanContentLength > (spanWidth - 50)) {
-                // newSpanContent = newSpanContent + "<br>"
-                spanContentLength = 0
-                console.log("insert br, span width: ", spanWidth, ", span content width: ", spanContentLength)
-            }
-
-            // get index of ',' for wordRaw
-            let commaIndex = wordInRawParagraph.indexOf(',')
-            // if comma exists and comma is not the first or the last char
-            if (commaIndex > 0 && commaIndex < wordInRawParagraph.length - 1) {
-                // append space for comma
-                wordInRawParagraph = wordInRawParagraph.replace(",", ", ")
-            }
-            wordInRawParagraph = wordInRawParagraph.trim()
-            rawParagraphWordArray = wordInRawParagraph.split(' ')
-            console.log("raw words: ", rawParagraphWordArray)
-            for (let wordTmp of rawParagraphWordArray) {
-                let word = wordTmp.replace(/[^a-zA-Z\-]/g, '');
-                if (word.length === 0) {
-                    continue
-                }
-                if (word in response.wordProperties) {
-                    let ecp = response.wordProperties[word]
-                    console.log("word: ", word, "ecp: ", ecp)
-                    let loadCount = ecp.LoadCount
-                    console.log("word: ", word, " load count: ", loadCount)
-                    let colorCode = getColorCodeByCount(ecp)
-                    console.log("word: ", word, ", load count: ", loadCount, ", color code: ", colorCode)
-                    let startTag = '<u alt="alt-foo" onclick="popEnxDialogBox(event)" class="class-foo" style="text-decoration: #000000 underline; text-decoration-thickness: 2px;">'
-                    startTag = startTag.replace("#000000", colorCode);
-                    startTag = startTag.replace("class-foo", "enx-" + ecp.Key);
-                    startTag = startTag.replace("alt-foo", ecp.Key);
-                    newSpanContent = newSpanContent + startTag + wordTmp + '</u> '
-                } else {
-                    newSpanContent = newSpanContent + ' ' + wordTmp + ' '
-                }
-                spanContentLength = spanContentLength + word.length * 8 + wordMargin
-                console.log("span content width: ", spanContentLength)
-            }
-        }
-        parentNode.innerHTML = newSpanContent
-    })();
-}
-
 function getArticleNode() {
     // gofluent
     let articleClassElement = document.getElementsByClassName("Article")
@@ -285,64 +139,111 @@ function enxRun() {
         try {
             const src = chrome.runtime.getURL("content_module.js");
             const contentMain = await import(src);
-            let nodeList = contentMain.findChildNodes0(articleNode);
+            let nodeList = contentMain.findChildNodes(articleNode);
             console.log("node list: ", nodeList)
-            for (let articleNode of nodeList){
+            for (let tmpNode of nodeList){
                 (async () => {
                     console.log("sending msg from content script to backend")
-                    let oneParagraph = articleNode.paragraph
+                    let oneParagraph = tmpNode.paragraph
                     const response = await chrome.runtime.sendMessage({ msgType: "getWords", words: oneParagraph });
                     // do something with response here, not outside the function
                     console.log("response from backend: ", response)
                     console.log(response.wordProperties);
-            
-                    let newSpanContent = ""
-                    let spanContentLength = 0
-                    let wordMargin = 4;
-            
-                    wordsInParagraph = oneParagraph.split(' ')
-                    for (let wordInRawParagraph of wordsInParagraph) {
-                        if (spanContentLength > 0 && spanWidth > 50 && spanContentLength > (spanWidth - 50)) {
-                            // newSpanContent = newSpanContent + "<br>"
-                            spanContentLength = 0
-                            console.log("insert br, span width: ", spanWidth, ", span content width: ", spanContentLength)
-                        }
-            
-                        // get index of ',' for wordRaw
-                        let commaIndex = wordInRawParagraph.indexOf(',')
-                        // if comma exists and comma is not the first or the last char
-                        if (commaIndex > 0 && commaIndex < wordInRawParagraph.length - 1) {
-                            // append space for comma
-                            wordInRawParagraph = wordInRawParagraph.replace(",", ", ")
-                        }
-                        wordInRawParagraph = wordInRawParagraph.trim()
-                        rawParagraphWordArray = wordInRawParagraph.split(' ')
-                        console.log("raw words: ", rawParagraphWordArray)
-                        for (let wordTmp of rawParagraphWordArray) {
-                            let word = wordTmp.replace(/[^a-zA-Z\-]/g, '');
-                            if (word.length === 0) {
-                                continue
-                            }
-                            if (word in response.wordProperties) {
-                                let ecp = response.wordProperties[word]
-                                console.log("word: ", word, "ecp: ", ecp)
-                                let loadCount = ecp.LoadCount
-                                console.log("word: ", word, " load count: ", loadCount)
-                                let colorCode = getColorCodeByCount(ecp)
-                                console.log("word: ", word, ", load count: ", loadCount, ", color code: ", colorCode)
-                                let startTag = '<u alt="alt-foo" onclick="popEnxDialogBox(event)" class="class-foo" style="text-decoration: #000000 underline; text-decoration-thickness: 2px;">'
-                                startTag = startTag.replace("#000000", colorCode);
-                                startTag = startTag.replace("class-foo", "enx-" + ecp.Key);
-                                startTag = startTag.replace("alt-foo", ecp.Key);
-                                newSpanContent = newSpanContent + startTag + wordTmp + '</u> '
-                            } else {
-                                newSpanContent = newSpanContent + ' ' + wordTmp + ' '
-                            }
-                            spanContentLength = spanContentLength + word.length * 8 + wordMargin
-                            console.log("span content width: ", spanContentLength)
-                        }
+
+                    tmpInnerHtml = tmpNode.node.innerHTML
+                    if (tmpInnerHtml === undefined){
+                        return
                     }
-                    articleNode.node.innerHTML = newSpanContent
+                    newInnerHtml = ""
+                    
+
+                    htmlTag = false
+                    tagIndexStart = undefined
+                    tagIndexStop = undefined
+                    tagName = undefined
+
+                    copyIndex = 0
+                    wordIndexStart = undefined
+                    wordIndexEnd = undefined
+                    stringIndexStart = 0
+                    stringIndexEnd = 1
+
+                    index =0
+                    while (index<tmpInnerHtml.length){
+                        let oneCharacter = tmpInnerHtml.charAt(index)
+                        var oneCharacterCode = oneCharacter.charCodeAt();
+                        //tmpString = tmpInnerHtml.slice(stringIndexStart,stringIndexEnd)
+
+
+                        if((oneCharacterCode >= 65 && oneCharacterCode <= 90)||(oneCharacterCode >= 97 && oneCharacterCode <= 122)){
+                            // english character
+                            if (htmlTag === false && wordIndexStart === undefined){
+                                wordIndexStart = index
+                            }
+                        }else if (oneCharacter==="-" || oneCharacter==="'"){
+                            // do nothing
+                        }else{
+                            if (htmlTag === false &&  wordIndexStart !== undefined && wordIndexEnd === undefined ){
+                                wordIndexEnd = index
+                            }
+                            
+                            if (tagName===undefined){
+                                //if (oneCharacter==="<" && tmpInnerHtml.slice(index,index+2) === "<a"){
+                                if (oneCharacter==="<"){
+                                    htmlTag = true
+                                    tagIndexStart = index+1
+                                }
+                                //if (htmlTag === true && oneCharacter === ">" && tmpInnerHtml.slice(index-1,index+1)==="a>"){
+                                if (htmlTag === true && (oneCharacter === " "|| oneCharacter===">")){
+                                    //htmlTag = false
+                                    tagName = tmpInnerHtml.slice(tagIndexStart,index)
+                                    console.log("tag name:", tagName)
+                                }
+                            }else{
+                                console.log("check html tag close:", tmpInnerHtml.slice(index-tagName.length,index+1))
+                                if (oneCharacter === ">" && tmpInnerHtml.slice(index-tagName.length,index+1)===tagName+">"){
+                                    htmlTag = false
+                                    tagName = undefined
+                                }
+                            }
+                        }
+                        console.log("index: ",index,"one character:", oneCharacter, "slice 0:",  tmpInnerHtml.slice(index,index+2), "slice 1:",tmpInnerHtml.slice(index-1,index+1),", html tag:", htmlTag, ", start: ", wordIndexStart,", end: ", wordIndexEnd)
+                        index = index + 1
+                        
+                        if (wordIndexStart===undefined || wordIndexEnd===undefined){
+                            continue
+                        }
+
+                        // found one word
+                        tmpWord = tmpInnerHtml.slice(wordIndexStart,wordIndexEnd)
+                        if (tmpWord in response.wordProperties) {
+                            let ecp = response.wordProperties[tmpWord]
+                            console.log("word: ", tmpWord, "ecp: ", ecp)
+                            let loadCount = ecp.LoadCount
+                            let colorCode = getColorCodeByCount(ecp)
+                            let startTag = '<u alt="alt-foo" onclick="popEnxDialogBox(event)" class="class-foo" style="text-decoration: #000000 underline; text-decoration-thickness: 2px;">'
+                            startTag = startTag.replace("#000000", colorCode);
+                            startTag = startTag.replace("class-foo", "enx-" + ecp.Key);
+                            startTag = startTag.replace("alt-foo", ecp.Key);
+                            newInnerHtml = newInnerHtml + tmpInnerHtml.slice(copyIndex, wordIndexStart)
+                            newInnerHtml = newInnerHtml + startTag + tmpWord + '</u> '
+                            copyIndex = wordIndexEnd
+                            wordIndexStart = undefined
+                            wordIndexEnd = undefined
+                        }else{
+                            console.log("word not found: ", tmpWord)
+                            newInnerHtml = newInnerHtml + tmpInnerHtml.slice(copyIndex, wordIndexStart)
+                            newInnerHtml = newInnerHtml + ' ' + tmpWord + ' '
+                            wordIndexStart = undefined
+                            wordIndexEnd = undefined
+                        }
+                  
+                    
+                    }
+                    newInnerHtml = newInnerHtml + tmpInnerHtml.slice(copyIndex)
+                    tmpNode.node.innerHTML = newInnerHtml
+                    console.log("original html: ", tmpInnerHtml)
+                    console.log("new html: ", newInnerHtml)
                 })();
             }
             // multiple content js test
--- chrome-enx/content_module.js
+++ chrome-enx/content_module.js
@@ -11,106 +11,45 @@ export function createOneArticleNode(){
 
 let spanWidth = 0;
 
-export function findChildNodes0(parentNode){
+export function findChildNodes(parentNode){
     let nodeList = []
-
-    console.log("find child node 0, parent node: ", parentNode)
+    console.log("parent node: ", parentNode)
+    console.log("find child node, parent node: ", parentNode)
     let childNodes = parentNode.childNodes
 
     console.log("child node count: ", childNodes.length)
     if (childNodes.length === 0) {
-        console.log("child node count ==0 return")
+        console.log("child node count == 0 return")
         return nodeList
     }
 
-    // check if article context
-    let articleContent = true
-    for (let childNode of childNodes) {
-        let childNodeTagName = childNode.tagName
-        console.log("child node: ", childNode, ", tag name: ", childNodeTagName)
-
-        // if child node only contains A, CODE ..., this node is article content node
-        if (childNodeTagName !== "A" && childNodeTagName !== undefined && childNodeTagName !== "EM" && childNodeTagName !== "CODE") {
-            // has sub article node
-            articleContent = false
-            console.log("article content: ", articleContent)
-            break
+    var textContent = '';
+    childNodes.forEach(function (tmpNode) {
+        if (tmpNode.nodeType === 3) {
+            if (tmpNode.textContent.trim() !== ""){
+                textContent += tmpNode.textContent.trim() + ' ';
+            }
         }
+    });
+
+    console.log("outer div content: ", textContent, ", length: ", textContent.length);
+
+    if (textContent === ""){
+        childNodes.forEach(function (tmpNode) {
+            let tmpNodeType = tmpNode.nodeType
+            let tmpNodeTagName = tmpNode.tagName
+            console.log("child node: ",tmpNode, ", tag: ", tmpNodeTagName, ", type: ", tmpNodeType)
+            if (tmpNodeType === 1){
+                if (tmpNodeTagName.toUpperCase() === "P" || tmpNodeTagName.toUpperCase() === "BLOCKQUOTE"){
+                    let tmp_list = findChildNodes(tmpNode)
+                    nodeList = nodeList.concat(tmp_list)
+                }
+            }
+        });
+    }else{
+        let tmp_node = new ArticleNode(parentNode, textContent)
+        nodeList.push(tmp_node)
+        
     }
-
-    let index = 0
-    if (articleContent === false) {
-        console.log("article node ==false, check sub node")
-        for (let node of childNodes) {
-            console.log("check child node, index: ", index)
-            let tmp_list = findChildNodes0(node)
-            nodeList = nodeList.concat(tmp_list)
-            index = index + 1
-        }
-        return nodeList
-    }
-
-
-    let currentTagName = parentNode.currentTagName
-    console.log("current tag: ", currentTagName, "node: ", parentNode)
-
-    console.log("span width: ", parentNode.getBoundingClientRect().width)
-    if (parentNode.getBoundingClientRect().width > spanWidth) {
-        spanWidth = parentNode.getBoundingClientRect().width
-    }
-
-    let spanContent = parentNode.innerHTML
-    let oneParagraph = parentNode.textContent
-
-    if (oneParagraph == undefined) {
-        console.log('paragraph is undefined')
-        return
-    }
-    // remove duplicate whitespace
-    oneParagraph = oneParagraph.replace(/\s+/g, ' ')
-    console.log('inner text: ', oneParagraph)
-    console.log('span inner html: ', spanContent)
-    console.log("span width: ", spanWidth)
-    // remove <a> tag
-    let filteredContent = ""
-    let tagAppeared = false
-    let collectWord = true
-    for (let i = 0; i < spanContent.length; i++) {
-        if (!tagAppeared) {
-            collectWord = true
-        }
-        let c = spanContent.charAt(i)
-        if (c === "<") {
-            // tag start
-            tagAppeared = true
-        } else if (c === ">") {
-            tagAppeared = false
-        }
-        if (tagAppeared) {
-            collectWord = false
-        }
-        if (collectWord) {
-            filteredContent += c
-        }
-    }
-    console.log("span inner html remove tag: ", filteredContent)
-    let words = filteredContent.split(' ')
-
-
-    let re = /^[0-9a-zA-Z-,.']+$/;
-
-    let rawParagraphWordArray = [];
-
-    for (let word of words) {
-        word = word.replace(",", "");
-        word = word.replace(".", "");
-        if (re.test(word)) {
-            rawParagraphWordArray.push(word)
-        }
-    }
-
-    let tmp_node = new ArticleNode(parentNode, oneParagraph)
-    nodeList.push(tmp_node)
-
     return nodeList
 }
