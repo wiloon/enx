d1b99ae3d2687cf75235d224617616cab8ec233a
Yue <wiloon.wy@gmail.com>
2024-10-06 20:17:56 +0800

    build test

--- enx-api/deploy/deploy-jenkins.sh
+++ enx-api/deploy/deploy-jenkins.sh
@@ -17,13 +17,13 @@ echo "stop service"
 
 # copy systemd unit file to local server
 ls -l ~/.ssh/id_ed25519
-ansible -i '192.168.50.36,' all -m copy -a 'src=/var/lib/jenkins/workspace/enx-api/enx-api/deploy/enx-api.service dest=/etc/systemd/system/enx-api.service' -u root
-ansible -i '192.168.50.36,' all -m shell -a 'systemctl daemon-reload' --key-file ~/.ssh/id_ed25519
-ansible -i '192.168.50.36,' all -m shell -a 'systemctl stop enx-api' --key-file ~/.ssh/id_ed25519
-scp -i ~/.ssh/id_ed25519 ${package_name} root@192.168.50.36:/usr/local/bin
+ansible -i '192.168.50.36,' all -m copy -a 'src=/var/lib/jenkins/workspace/enx-api/enx-api/deploy/enx-api.service dest=/etc/systemd/system/enx-api.service' -u=root
+ansible -i '192.168.50.36,' all -m shell -a 'systemctl daemon-reload' -u=root
+ansible -i '192.168.50.36,' all -m shell -a 'systemctl stop enx-api' -u=root
+scp -i ${package_name} root@192.168.50.36:/usr/local/bin
 
-ansible -i '192.168.50.36,' all -m file -a 'path=/usr/local/etc/enx/ state=directory mode=0755' --key-file ~/.ssh/id_ed25519
-ansible -i '192.168.50.36,' all -m file -a 'path=/var/lib/enx-api/ state=directory mode=0755' --key-file ~/.ssh/id_ed25519
+ansible -i '192.168.50.36,' all -m file -a 'path=/usr/local/etc/enx/ state=directory mode=0755' -u=root
+ansible -i '192.168.50.36,' all -m file -a 'path=/var/lib/enx-api/ state=directory mode=0755' -u=root
 
 scp -i ~/.ssh/id_ed25519 config.toml root@192.168.50.36:/usr/local/etc/enx/config.toml
 
@@ -33,5 +33,5 @@ rm -f ${package_name}
 echo "start service"
 
 # local server
-ansible -i '192.168.50.36,' all  -m shell -a 'systemctl enable enx-api' --key-file ~/.ssh/id_ed25519
-ansible -i '192.168.50.36,' all  -m shell -a 'systemctl restart enx-api' --key-file ~/.ssh/id_ed25519
+ansible -i '192.168.50.36,' all  -m shell -a 'systemctl enable enx-api' -u=root
+ansible -i '192.168.50.36,' all  -m shell -a 'systemctl restart enx-api' -u=root
