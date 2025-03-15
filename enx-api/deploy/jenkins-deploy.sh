fd8900d48d4d5072c29683f2b162f30f9de92662
Yue <wiloon.wy@gmail.com>
2025-03-15 13:04:33 +0800

    jenkins deploy

--- enx-api/deploy/jenkins-deploy.sh
+++ enx-api/deploy/jenkins-deploy.sh
@@ -47,7 +47,7 @@ ansible -i '192.168.50.36,' all  -m shell -a 'systemctl start enx-api' -u=root
 # test api
 curl http://192.168.50.36:8080/ping
 
-ls -l /var/lib/jenkins/.cache/go-build
+find /var/lib/jenkins/.cache/go-build -mtime +7 -type f -name "*.*"
 
 echo ""
 echo "done"
